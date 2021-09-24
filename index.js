b7be29b8bc82d335879189b9234dd94a3f657459
Joshman <ejembijoshman@gmail.com>
2021-09-24 12:27:04 +0100

    final push updates

--- index.js
+++ index.js
@@ -69,57 +69,6 @@ function checkButton() {
 }
 
 
-// function loanAmount() {
-//     // initializing the value inputed as loan to be given
-//     var loan = document.getElementById("lAmount").value;
-
-//     // to select preffered month duration
-//     var getSelectedValue = document.querySelector('input[name="month"]:checked');
-
-//     // to calculate for respective loan type
-//     var interest = document.getElementById('interest').value;
-
-//     var int = interest / 100;
-
-//     // to calculate the interest rate on loan collected
-//     // var int = (4.25 / 100);
-
-//     const rat = document.getElementById("rate").innerHTML = (loan * int).toFixed(2);
-
-//     // for the six month tenure
-//     if (getSelectedValue != null) {
-        
-//         var first = document.getElementById("six").value;
-        
-//         // to calculate principal value for six month
-//         const fCalc = document.getElementById('principal').innerHTML = (loan / first).toFixed(2);
-
-//         // monthly payment value
-//         document.getElementById('show').innerHTML = '₦ ' + ((loan * int) + (loan / first)).toFixed(2);
-//     } 
-//     else if (getSelectedValue != null) {
-
-//         var second = document.getElementById("nine").value;
-
-//         // to calculate principal value for nine month
-//         document.getElementById('principal').innerHTML = (loan / second).toFixed(2);
-
-//         // monthly payment value
-//         document.getElementById('show').innerHTML = '₦ ' + ((loan * int) + (loan / second)).toFixed(2);
-//     }
-//     else if (getSelectedValue != null) {
-
-//         var third = document.getElementById("twelve").value;
-
-//         // to calculate principal value for twelve month
-//         document.getElementById('principal').innerHTML = (loan / third).toFixed(2);
-
-//         // monthly payment value
-//         document.getElementById('show').innerHTML = '₦ ' + ((loan * int) + (loan / third)).toFixed(2);
-//     }
-// }
-
-
 // all codes written out by Ejembi joshua
 // github @joshman106
 // mail me @ ejembijoshman@gmail.com
